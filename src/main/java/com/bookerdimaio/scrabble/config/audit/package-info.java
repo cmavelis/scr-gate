9f6f648a85d242521e9de3a3f27f9b296e4e2892
camavelis <cmavelis@gmail.com>
2019-07-29 10:29:06 -0400

    Initial application generated by JHipster-6.1.2

---
+++ src/main/java/com/bookerdimaio/scrabble/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.bookerdimaio.scrabble.config.audit;
---
+++ src/main/java/com/bookerdimaio/scrabble/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.bookerdimaio.scrabble.web.rest;
