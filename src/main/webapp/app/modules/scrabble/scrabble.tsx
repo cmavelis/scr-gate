79858dcbddb47925debbf33e002f46bce0e22792
BDGHR <grant.reighard@bookerdimaio.com>
2019-07-30 16:16:36 -0400

    Removes word calculation file from branch.

--- src/main/webapp/app/modules/scrabble/scrabble.tsx
+++ src/main/webapp/app/modules/scrabble/scrabble.tsx
@@ -2,7 +2,6 @@ import './scrabble.scss';
 
 import React from 'react';
 import SelectNumber from './selectNumber';
-import WordCalculation from './wordCalculation';
 
 export default class Scrabble extends React.Component {
   render() {
@@ -10,7 +9,6 @@ export default class Scrabble extends React.Component {
       <div className="main">
         Something, not nothing!
         <SelectNumber />
-        <WordCalculation />
       </div>
     );
   }
