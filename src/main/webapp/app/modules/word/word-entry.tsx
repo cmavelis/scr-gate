e8d84668b1faa8cd29acdef862254a494d5c388c
camavelis <cmavelis@gmail.com>
2019-08-01 17:33:42 -0400

    storing letters individually in state

--- src/main/webapp/app/modules/word/word-entry.tsx
+++ src/main/webapp/app/modules/word/word-entry.tsx
@@ -1,18 +1,25 @@
 import './word-entry.scss'; // TODO: remove local stylesheet
 
 import React from 'react';
+import { range } from 'lodash';
 
-import { Button, Col, Container, Input, Row } from 'reactstrap';
+import { Button, Col, Container, Input, InputGroup, Row } from 'reactstrap';
 
 export interface IWordState {
-  word: string;
+  word: {
+    [key: number]: string;
+  };
 }
+
+const fifteen = range(15).map(n => '');
 
 export class WordEntry extends React.Component<{}, IWordState> {
   constructor(props) {
     super(props);
     this.state = {
-      word: 'word'
+      word: {
+        ...fifteen
+      }
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -20,11 +27,12 @@ export class WordEntry extends React.Component<{}, IWordState> {
 
   handleChange(event, index) {
     const { value } = event.target;
-    this.setState(prevState => {
-      const word = prevState.word;
-      const newWord = word.substr(0, index) + value + word.substr(index + 1);
-      return { word: newWord };
-    });
+    this.setState(prevState => ({
+      word: {
+        ...prevState.word,
+        [index]: value
+      }
+    }));
   }
 
   render() {
@@ -34,21 +42,22 @@ export class WordEntry extends React.Component<{}, IWordState> {
         <Button color="info">All 7 tiles played</Button>
         <Container>
           <Row className="justify-content-start no-gutters">
-            {'word'.split('').map((letter, index) => (
-              <Col key={`col`} className="col-1">
+            <InputGroup word={Object.values(word)}>
+              {'123456789012345'.split('').map((letter, index) => (
                 <Input
                   key={`letter-${letter}`}
                   className="letter-input"
                   // tslint:disable-next-line:jsx-no-lambda
                   onChange={e => this.handleChange(e, index)}
                   value={word[index]}
+                  disabled={index > 1 && word[index - 1] === ''}
                 >
                   {letter}
                 </Input>
-              </Col>
-            ))}
+              ))}
+            </InputGroup>
           </Row>
-          <Row>{this.state.word}</Row>
+          <Row>{Object.values(this.state.word)}</Row>
         </Container>
       </div>
     );
