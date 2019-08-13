3f89186abfd175581b86037a2c09acb3647de785
camavelis <cmavelis@gmail.com>
2019-08-13 17:21:20 -0400

    removing old old entities

--- src/main/webapp/app/shared/model/game/player.model.ts
+++
@@ -1,9 +0,0 @@
-import { IGame } from 'app/shared/model/game/game.model';
-
-export interface IPlayer {
-  id?: number;
-  name?: string;
-  games?: IGame[];
-}
-
-export const defaultValue: Readonly<IPlayer> = {};
